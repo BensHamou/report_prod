728bbe2eeb0d27b5195b048e3c3b9b774b977fe3
mohammed.benslimane <mohammed.benslimane@groupe-hasnaoui.com>
2023-07-19 11:13:06 +0100

    Settings

--- report_prod/settings.py
+++ report_prod/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTHENTICATION_BACKENDS = [    
@@ -8,9 +9,14 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     ]
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 AUTH_USER_MODEL = 'account.User'
@@ -19,9 +25,8 @@ swappable = 'AUTH_USER_MODEL'
 
 ADMIN_URL = 'puma_prod/admin/'
 
-ALLOWED_HOSTS = ['10.10.10.53']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'web']
 
-CSRF_COOKIE_SECURE = False
 
 # Application definition
 
@@ -76,17 +81,24 @@ TEMPLATES = [
 WSGI_APPLICATION = "report_prod.wsgi.application"
 
 
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'prod_report',
+        'USER': 'puma_u',
+        'PASSWORD': 'puma_u',
+        'HOST': '192.168.56.1',
+        'PORT': '5432',
     }
 }
 
+        #'HOST': '192.168.56.1',
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,6 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = "fr"
 
@@ -113,11 +127,21 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = '/static'
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
