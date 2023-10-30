845c0d977f53111855f7cceac90e02c734c01adb
mohammed.benslimane <benshamou@gmail.com>
2023-10-30 17:11:43 +0100

    edit cron

--- report/cron.py
+++ report/cron.py
@@ -7,15 +7,15 @@ def send_alert():
     
     # get all lines that have include_cron = True
     lines = Line.objects.filter(include_cron=True)
-    current_time = datetime.now().time().strftime('%H:%M:%S')
+    current_time = datetime.now()
     today = datetime.today().date()
 
     for line in lines:
         for shift in line.shifts():
             shift_end_time = time(shift.hour_end, shift.minutes_end)
             allowed_delay = timedelta(minutes=line.allowed_delay)
-            threshold = (datetime.combine(datetime.today(), shift_end_time) + allowed_delay).time().strftime('%H:%M:%S')
-            threshold_limit = (datetime.combine(datetime.today(), shift_end_time) + allowed_delay + timedelta(minutes=120)).time().strftime('%H:%M:%S')
+            threshold = datetime.combine(datetime.today(), shift_end_time) + allowed_delay
+            threshold_limit = datetime.combine(datetime.today(), shift_end_time) + allowed_delay + timedelta(minutes=120)
 
             if threshold <= current_time <= threshold_limit:
                 alert = True
